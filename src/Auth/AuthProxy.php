d4f2bd5303fa032a352a474eb438ba46d396eaa8
Jogi Silalahi <jsilalahi@dyned.com>
2020-03-05 13:35:16 +0700

    wip

---
+++ src/Auth/AuthProxy.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Neo\Auth;
+
+class AuthProxy
+{
+    /**
+     * Flagging to retrieve which server is found from last api call
+     *
+     * @var string
+     */
+    protected $lastServerFound = "";
+
+    /**
+     * List of auth shadowing SSO servers
+     *
+     * @var array Auth
+     */
+    protected $auths = [];
+
+    /**
+     * AuthMultiServer constructor
+     *
+     * @param array $httpClients
+     * @param array $endpoints
+     * @param array $configs
+     */
+    public function __construct(array $httpClients, array $endpoints, array $configs)
+    {
+        foreach ($httpClients as $key => $httpClient) {
+            $this->auths[$key] = new Auth(
+                $httpClient,
+                $endpoints[$key],
+                $configs[$key]
+            );
+        }
+    }
+
+    /**
+     * Retrieve token from SSO services based on given credential.
+     *
+     * @param array $credential
+     *
+     * @return Token|null
+     */
+    public function token(array $credential)
+    {
+        foreach ($this->auths as $server => $auth) {
+            if($result = $auth->token($credential)) {
+                $this->lastServerFound = $server;
+                return $result;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Verify given token from SSO services.
+     *
+     * @param Token $token
+     *
+     * @return bool
+     */
+    public function verify(Token $token)
+    {
+        foreach ($this->auths as $server => $auth) {
+            if($result = $auth->verify($token)) {
+                $this->lastServerFound = $server;
+                return $result;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Retrieve user info based on given token.
+     *
+     * @param Token $token
+     *
+     * @return mixed|null
+     */
+    public function user(Token $token)
+    {
+        foreach ($this->auths as $server => $auth) {
+            if($result = $auth->user($token)) {
+                $this->lastServerFound = $server;
+                return $result;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Login authorize given credential and returns user (with token, acl and profile).
+     *
+     * @param array $credential
+     *
+     * @return User|false
+     */
+    public function login(array $credential)
+    {
+        foreach ($this->auths as $server => $auth) {
+            if($result = $auth->login($credential)) {
+                $this->lastServerFound = $server;
+                return $result;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Retrieve last server found
+     *
+     * @return string
+     */
+    public function getLastServerFound()
+    {
+        return $this->lastServerFound;
+    }
+}
---
+++ tests/AuthMultiServerTest.php
@@ -0,0 +1,153 @@
+<?php
+
+use Neo\Auth\Auth;
+use Neo\Auth\Token;
+use Neo\Exceptions\ValidationException;
+use Neo\HttpClients\GuzzleHttpClient;
+use PHPUnit\Framework\TestCase;
+use Tightenco\Collect\Support\Collection;
+
+class AuthMultiServerTest extends TestCase
+{
+    protected $baseUriGlobal;
+    protected $baseUriChina;
+    protected $username;
+    protected $password;
+    protected $auth;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->baseUriGlobal = getenv('NEO_SSO_BASE_URI_GLOBAL');
+        $this->baseUriChina = getenv('NEO_SSO_BASE_URI_CHINA');
+        $this->username = getenv('NEO_SSO_USERNAME');
+        $this->password = getenv('NEO_SSO_PASSWORD');
+
+        $httpClient = new GuzzleHttpClient([
+            'base_uri' => getenv('NEO_SSO_BASE_URI'),
+            'timeout'  => 10,
+        ]);
+
+        $this->auth = new Auth($httpClient);
+    }
+
+    public function testAuthTokenValidation_EmptyCredential()
+    {
+        $this->expectException(
+            ValidationException::class
+        );
+
+        $this->auth->token([]);
+    }
+//
+//    public function testAuthTokenValidation_InvalidUsername()
+//    {
+//        $token = $this->auth->token([
+//            'username' => 'invalid',
+//            'password' => $this->password,
+//        ]);
+//
+//        $this->assertNull($token);
+//    }
+//
+//    public function testAuthTokenValidation_PasswordNotMatch()
+//    {
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => 'invalid',
+//        ]);
+//
+//        $this->assertNull($token);
+//    }
+//
+//    public function testAuthTokenCredential()
+//    {
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $this->assertInstanceOf(Token::class, $token);
+//        $this->assertIsString($token->string());
+//    }
+//
+//    public function testAuthTokenCredential_RawResponse()
+//    {
+//        $this->auth->setConfig('raw_response', false);
+//
+//        $this->assertEquals(false, $this->auth->getConfig('raw_response'));
+//
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $this->assertInstanceOf(Token::class, $token);
+//        $this->assertIsString($token->string());
+//
+//        $this->auth->setConfig('raw_response', true);
+//
+//        $this->assertEquals(true, $this->auth->getConfig('raw_response'));
+//
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $this->assertIsString($token);
+//
+//        $this->auth->setConfig(['raw_response' => false]);
+//
+//        $this->assertEquals(false, $this->auth->getConfig('raw_response'));
+//    }
+//
+//    public function testAuthTokenVerify_Invalid()
+//    {
+//        $valid = $this->auth->verify(
+//            new Token('invalid')
+//        );
+//
+//        $this->assertFalse($valid);
+//    }
+//
+//    public function testAuthTokenVerify_Valid()
+//    {
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $valid = $this->auth->verify($token);
+//
+//        $this->assertTrue($valid);
+//    }
+//
+//    public function testAuthUser()
+//    {
+//        $token = $this->auth->token([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $user = $this->auth->user($token);
+//
+//        $this->assertObjectHasAttribute('acl', $user);
+//        $this->assertObjectHasAttribute('profile', $user);
+//    }
+//
+//    public function testAuthLogin()
+//    {
+//        $user = $this->auth->login([
+//            'username' => $this->username,
+//            'password' => $this->password,
+//        ]);
+//
+//        $this->assertTrue($this->auth->verify($user->token()));
+//        $this->assertInstanceOf(Token::class, $user->token());
+//        $this->assertInstanceOf(Collection::class, $user->acl());
+//        $this->assertInstanceOf(Collection::class, $user->profile());
+//        $this->assertEquals('super_admin', $user->profile()->get('roles')[0]);
+//        $this->assertEquals('super_admin', $user->profile('roles')[0]);
+//    }
+}
